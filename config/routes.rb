ddfa3c4fa204cfd579c834f683da65a30ea222bc
Rafael Mateus <rafaelmateus@hotmail.com.br>
2018-11-28 13:53:20 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
-    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1) do
-      resources :tasks
-    end
-
-    namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2, default: true) do
-      resources :tasks
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
     end
   end
 end
