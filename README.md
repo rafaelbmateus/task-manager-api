fb6ed2d43b82b29454234a420629165ef06d755a
Rafael Mateus <rafaelmateus@hotmail.com.br>
2018-11-28 16:35:56 +0000

    adding create action to the users controller with tests

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ rails s -b 0.0.0.0 -p 3001
 
 127.0.0.1 api.task-manager.test
 ```
+
+# Curls
+## Show User
+curl -v -H "Accpet: application/vnd.taskmanager.v1" http://api.task-manager.test:3001/users/1
+
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,29 +4,62 @@ RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
   let(:user_id) { user.id }
 
-  before { host! "api.taskmanager.test" }
+  before { host! 'api.taskmanager.test' }
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
     before do
-      headers = { "Accept" => "application/vnd.taskmanager.v1" }
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
-    context "when the user exists" do
-      it "returns the users" do
+    context 'when the user exists' do
+      it 'returns the users' do
         user_response = JSON.parse(response.body)
-        expect(user_response["id"]).to eq(user_id)
+        expect(user_response['id']).to eq(user_id)
       end
 
-      it "returns status 200" do
+      it 'returns status 200' do
         expect(response).to have_http_status(200)
       end
     end
 
-    context "when the user not exists" do
+    context 'when the user not exists' do
       let(:user_id) { 10000 }
-      it "returns status 404" do
+      it 'returns status 404' do
         expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe 'POST /users' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      post "/users", params: {user: user_params }, headers: headers
+    end
+
+    context 'when the request params are valid' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns json data for created user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns the json data fot the errors' do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
       end
     end
   end
